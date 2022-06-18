27ebddd2375e064da57b9b231249ec8f6bae0504
feyza-ce <86958739+feyza-ce@users.noreply.github.com>
2022-06-18 17:53:49 +0300

    Add project files.

---
+++ ConsoleApp2/Data.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Data
+    {
+        public string nam = "fy";
+        // I have to define to variables
+        // In this page we have different arrays one of them for customers informations
+        // The other array for our product information
+        // The first array is the customers. We have string variables in here. 
+        // We have customer name, surname, id, phone and e-mail
+        public string[,] customers = {
+            { "Feyza", "Yılmaz", "01234569874", "545 100 00 00", "feyza.yilmaz01@mlia.com" },
+            { "Cem", "Tiryaki", "02234569874", "545 200 00 00", "cem.tiyaki02@mlia.com" },
+            { "Vito", "Konuk", "03234569874", "545 300 00 00", "vito.konuk03@mlia.com" },
+            { "Afra", "Yılmaz", "04234569874", "545 400 00 00", "afra.yilmaz04@mlia.com" },
+            { "Şevval", "Çetin", "05234569874", "545 500 00 00", "sevval.cetin05@mlia.com" }
+            //name,     surname,      id,         phone,         email
+        };
+
+        // The second array is the products. We have int variables in here.
+        // We have product nameid, price, quantity and volorid in here.
+        public int[,] products = {
+            {100, 545, 50, 01 },
+            {101, 200, 45, 02 },
+            {102, 250, 30, 02 },
+            {103, 349, 15, 03 },
+            {104, 650, 20, 05 },
+            {105, 449, 01, 04 }
+        //nameid,price,quantity,colorid
+        };
+        
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+
+
+namespace ConsoleApp2
+{
+    internal class Program : Data
+    {
+        // True id is 01234569874
+        static void Main(string[] args)
+        {
+            string id;
+            
+            // We used Instance
+            Data dta = new Data();
+            
+            StartUser:
+            string TotalBill;
+            Console.WriteLine("Hi User");
+            // Ask to user to ID number
+            Console.Write("Please, type your ID number : ");
+            id = Console.ReadLine();
+            if (id == null || id.Length != 11) // If id entered bu user is empty or its length is more than 11 user can not open the shopping part
+            {
+                Console.WriteLine("Please enter a valid ID number! The number you entered is" + id.Length + "character");
+            }
+
+
+            else if(id == dta.customers[0, 2])
+            {
+                Console.WriteLine("Welcome to our land please enjoy the shopping <3");
+                // We showed the products
+                Console.WriteLine("Our products are as listed below :) You can choose according to the specified instructions");
+                Console.WriteLine("Enter the id of the product you want to select...");
+                StartProduct:
+                for (int i = 0; i < dta.products.Length/4; i++)
+                {
+                    
+                    // string name = (dta.products[0,0] == 100) ? "Sweatshirt" : "";
+                    string name = (dta.products[i, 0] == 100) ? "Sweatshirt" : (dta.products[i, 0] == 101) ? "Pantolon" : (dta.products[i, 0] == 102) ? "Ceket" : (dta.products[i, 0] == 103) ? "Bluz" : (dta.products[i, 0] == 104) ? "Ayakkabı" : (dta.products[i, 0] == 105) ? "Mont" : "Giysi";
+                        
+                        Console.Write("{0} {1} Product ID = {2}",name ,dta.products[i,1], dta.products[i,0]);
+
+                        
+                    
+                    Console.WriteLine();
+
+                }
+                string decision;
+                string selectid ;
+                Console.Write("Enter the product id : ");
+                selectid = Console.ReadLine();
+                Console.WriteLine("Would you like to buy another product? Y - N");
+                decision = Console.ReadLine();
+                if (decision == "Y" || decision == "y")
+                {
+                    goto StartProduct;
+                }
+                else if (decision == "N" || decision == "n")
+                {
+                    Console.WriteLine("Your shop is done. Please message to accounting");
+                }
+
+            }
+            else // User is asked to sign up
+            {
+                string key;
+                Console.WriteLine("Sorry, You don't have any membership :( But you can join our family... ");
+                Console.WriteLine("Pleasee say YES 💍");
+                Console.WriteLine("Y or N");
+                key = Console.ReadLine();
+                if (key == "Y" || key == "y")
+                {
+                    Console.WriteLine("Welcome to our land please enjoy the shopping <3");
+                }
+                else if(key == "N" || key == "n")
+                {
+                    Console.WriteLine("Maybe another life...");
+                    Console.WriteLine("I can't keep you here forever. So if you want we can make a fresh start (Press Y)" + "If you want to break up with me... (Press N)");
+                    key = Console.ReadLine();
+                    if (key == "Y" || key == "y")
+                    {
+                        goto StartUser;
+                    }
+                    else if (key == "N" || key == "n")
+                    {
+                        Console.WriteLine("Please Press ENTER");
+                    }
+                }
+                
+            }
+            
+        }
+
+    }
+}
